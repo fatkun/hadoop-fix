209ebe3430ce008ba0b52c16d233fc5d25228b38
fatkun <fatkuns@gmail.com>
2015-04-22 10:06:57 +0800

    update pick.sh

--- hadoop/build_pick.sh
+++ hadoop/build_pick.sh
@@ -23,5 +23,6 @@ today=`date +%Y%m%d`
 
 cp hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-2.0.0-cdh4.2.1.jar  $BUILD_DIR/hadoop-hdfs-2.0.0-cdh4.2.1-${today}.jar 
 cp hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/target/hadoop-yarn-server-resourcemanager-2.0.0-cdh4.2.1.jar $BUILD_DIR/hadoop-yarn-server-resourcemanager-2.0.0-cdh4.2.1-${today}.jar
+cp hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/target/hadoop-mapreduce-client-core-2.0.0-cdh4.2.1.jar $BUILD_DIR/hadoop-mapreduce-client-core-2.0.0-cdh4.2.1-${today}.jar
 
 
